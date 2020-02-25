25f5a09d581eb17e6be8a36dbdb9d1ef298a7f9f
Anders Sølbech Larsen <asl@holstebro.dk>
2020-02-25 21:15:54 +0100

    first version of mysql exporter

---
+++ exporters/export_to_mysql.py
@@ -0,0 +1,49 @@
+import os
+import string
+import click
+import logging
+import mysqlx
+
+
+from kitos_helper.kitos_helper import KitosHelper
+import kitos_helper.kitos_logger as kl
+
+"""
+Exports datafrom KITOS  from to a local mysqlx compatible instance. 
+
+The data stored in the mysql database can be used with the web application "Softwareoversigten"
+"""
+
+logger = logging.getLogger("mysql-export")
+
+
+@click.command()
+@click.option('--prod', 'server', flag_value='https://kitos.dk', help='https://kitos.dk')
+@click.option('--test', 'server', flag_value='https://kitostest.miracle.dk', help='https://kitostest.miracle.dk')
+@click.option('--username', help='UserID for user with API access')
+@click.option('--password', help='Password for user')
+def export_to_mysql(server, username, password):
+
+    logger.info("Export to mysql started")
+
+    session = mysqlx.get_session({
+        'host': '10.1.21.163',
+        'port': 33060,
+        'user': 'kitos',
+        'password': 'kitos'
+    })
+
+    schema = session.get_schema('kitos')
+
+    table = mysqlx.Table(schema, 'cached_data')
+
+    kh = KitosHelper(username, password, server, False, True)
+    it_systems = kh.return_itsystems()
+
+    for it_id, it_system in it_systems.items():
+        it_org_usage = kh.return_isystem_usage(it_id)
+
+
+if __name__ == '__main__':
+    kl.start_logging("mysql_export.log")
+    export_to_mysql()
