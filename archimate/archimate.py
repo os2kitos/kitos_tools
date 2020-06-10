8b75b5750b771037fa1c79535c097e1b882362d2
Anders Sølbech Larsen <asl@holstebro.dk>
2020-06-10 19:47:24 +0200

    Added support for updating ids for elements

--- archimate/archimate.py
+++ archimate/archimate.py
@@ -1,102 +1,47 @@
-import json
 import os
+import json
 import pathlib
-import string
-import uuid
-
 import click
+
 
 import kitos_helper.kitos_logger as kl
 from kitos_helper.kitos_helper import KitosHelper
 
-from xml.dom.minidom import parse, parseString
+import archimate_helper as ah
 
 cfg_file = pathlib.Path.cwd() / 'settings' / 'settings.json'
 if not cfg_file.is_file():
     raise Exception('No setting file')
 
-SETTINGS = json.loads(cfg_file.read_text())
 
-
-class ArchimateHelper:
-    def __init__(self, archifile):
-        self.archifile = archifile
-        self.archi_dom = parse(self.archifile)
-
-        self.application_folder = None
-
-        folder_elements = self.archi_dom.getElementsByTagName('folder')
-
-        for folder in folder_elements:
-            if folder.getAttribute('type') == 'application':
-                self.application_folder = folder
-                break
-
-    def save(self):
-        writer = open(self.archifile, encoding='utf-8', mode='w')
-        self.archi_dom.writexml(writer)
-        writer.close()
-
-    def create_application_folder(self, folder_name):
-        newFolder = self.archi_dom.createElement("folder")
-        newFolder.setAttribute("name", folder_name)
-        newFolder.setAttribute("id", self._make_guid())
-        return newFolder
-
-    def _make_guid(self):
-        return str(uuid.uuid4())
-
-    def update_storm_structure(self):
-        for storm_category, storm_classes in SETTINGS['archimate.STORM'].items():
-            app_node = self.get_named_application_folder(storm_category)
-            if app_node == None:
-                app_node = self.create_application_folder(storm_category)
-                self.application_folder.appendChild(app_node)
-
-            for storm_class in storm_classes:
-                sub_app_node = self.get_named_application_folder(
-                    storm_class)
-                if sub_app_node == None:
-                    sub_app_node = self.create_application_folder(
-                        storm_class)
-                    app_node.appendChild(sub_app_node)
-
-    def get_named_application_folder(self, folder_name):
-        return self._find_childnode_from_attributes(self.application_folder, "folder", {"name": folder_name})
-
-    def _find_childnode_from_attributes(self, dom_node, element_name, attributes):
-        child_nodes = dom_node.getElementsByTagName(element_name)
-        child_node = None
-        for child in child_nodes:
-            if self.has_named_attributes(child, attributes):
-                child_node = child
-                break
-
-        return child_node
-
-    def _get_named_attribute(self, element, attribute_name):
-        return element.getAttributeNode(attribute_name)
-
-    def has_named_attributes(self, dom_node, attributes):
-        found = False
-        found_count = 0
-
-        for attr_name, attr_value in attributes.items():
-            if attr_value == dom_node.getAttribute(attr_name):
-                found_count += 1
-
-        if found_count == len(attributes):
-            found = True
-
-        return found
+SETTINGS = json.loads(cfg_file.read_text(encoding='utf-8'))
 
 
 @click.command()
-@click.option('--archifile', help='archimate file')
-def update_archimate_model(archifile):
-    ah = ArchimateHelper(archifile)
-    ah.update_storm_structure()
-    ah.save()
+@click.option('--infile', help='archimate file')
+@click.option('--outfile', help='archimate file')
+def update_archimate_model(infile, outfile):
+    archi_helper = ah.ArchimateHelper(infile, outfile)
+    archi_helper.update_storm_structure()
+
+    app_node = archi_helper.get_named_application_element("ITAL TEST")
+
+    cat_node = archi_helper.get_named_application_folder(
+        SETTINGS['archimate.STORM']["Web og kommunikation"][3])
+
+    if app_node == None:
+        new_element = archi_helper.create_application_element("ITAL TEST")
+        cat_node.appendChild(new_element)
+
+    app_node1 = archi_helper.get_named_application_element(
+        "Det Digitale Vandløbsregulativ (DVR)")
+    if app_node1 != None:
+        print(app_node1)
+
+    app_node = archi_helper.get_named_application_element("ITAL TEST")
+    archi_helper.update_application_id(app_node, "my_custom_id")
+
+    archi_helper.save(True)
 
 
 if __name__ == '__main__':
---
+++ archimate/archimate_helper.py
@@ -0,0 +1,135 @@
+import json
+import os
+import pathlib
+import string
+import uuid
+import codecs
+
+import kitos_helper.kitos_logger as kl
+
+from xml.dom.minidom import parse, parseString
+
+cfg_file = pathlib.Path.cwd() / 'settings' / 'settings.json'
+if not cfg_file.is_file():
+    raise Exception('No setting file')
+
+
+SETTINGS = json.loads(cfg_file.read_text(encoding='utf-8'))
+
+
+class ArchimateHelper:
+    def __init__(self, archifile, outfile):
+        self.archifile = archifile
+        self.outfile = outfile
+        self.archi_dom = parse(self.archifile)
+        self.replaced_ids = {}
+
+        self.application_folder = None
+
+        folder_elements = self.archi_dom.getElementsByTagName('folder')
+
+        for folder in folder_elements:
+            if folder.getAttribute('type') == 'application':
+                self.application_folder = folder
+                break
+
+    def save(self, strip_empty_lines=True):
+
+        temp_file = f"{self.outfile}.temp"
+
+        writer = open(temp_file, encoding='utf-8', mode='w')
+        self.archi_dom.writexml(writer, indent="\t", addindent="\t", newl="\n")
+        writer.close()
+
+        replace = True if len(self.replaced_ids) > 0 else False
+
+        if strip_empty_lines or replace:
+            with open(temp_file, encoding='utf-8', mode='r') as infile, open(self.outfile, encoding='utf-8', mode='w') as outfile:
+                for line in infile:
+                    if not line.strip():
+                        continue  # skip the empty line
+                    if replace:
+                        for cur_id, new_id in self.replaced_ids.items():
+                            line = line.replace(cur_id, new_id)
+                    outfile.write(line)  # non-empty line. Write it to output
+
+            outfile.close()
+            infile.close()
+
+        os.remove(temp_file)
+
+    def create_application_folder(self, folder_name, id=None):
+        if id == None:
+            id = self._make_guid()
+
+        newFolder = self.archi_dom.createElement("folder")
+        newFolder.setAttribute("name", folder_name)
+        newFolder.setAttribute("id", id)
+        return newFolder
+
+    def create_application_element(self, application_name, application_id=None):
+        if application_id == None:
+            application_id = self._make_guid()
+
+        newElement = self.archi_dom.createElement("element")
+        newElement.setAttribute("xsi:type", "archimate:ApplicationComponent")
+        newElement.setAttribute("name", application_name)
+        newElement.setAttribute("id", application_id)
+        return newElement
+
+    def update_application_id(self, element_node, application_id):
+        # read current id from element_node and assign new ID
+        # read entire XML file, search and replace style, for references
+        # to old id and replace them with the new.
+        current_id = element_node.getAttribute("id")
+        element_node.setAttribute("id", application_id)
+        self.replaced_ids.update({current_id: application_id})
+
+        return NotImplemented
+
+    def _make_guid(self):
+        return str(uuid.uuid4())
+
+    def update_storm_structure(self):
+        for storm_category, storm_classes in SETTINGS['archimate.STORM'].items():
+            app_node = self.get_named_application_folder(storm_category)
+            if app_node == None:
+                app_node = self.create_application_folder(storm_category)
+                self.application_folder.appendChild(app_node)
+
+            for storm_class in storm_classes:
+                sub_app_node = self.get_named_application_folder(
+                    storm_class)
+                if sub_app_node == None:
+                    sub_app_node = self.create_application_folder(
+                        storm_class)
+                    app_node.appendChild(sub_app_node)
+
+    def get_named_application_folder(self, folder_name):
+        return self._find_childnode_from_attributes(self.application_folder, "folder", {"name": folder_name})
+
+    def get_named_application_element(self, application_name):
+        return self._find_childnode_from_attributes(self.application_folder, "element", {"name": application_name, "xsi:type": "archimate:ApplicationComponent"})
+
+    def _find_childnode_from_attributes(self, dom_node, element_name, attributes):
+        child_nodes = dom_node.getElementsByTagName(element_name)
+        child_node = None
+        for child in child_nodes:
+            if self.has_named_attributes(child, attributes):
+                child_node = child
+                break
+
+        return child_node
+
+    def has_named_attributes(self, dom_node, attributes):
+        found = False
+        found_count = 0
+
+        for attr_name, attr_value in attributes.items():
+            if dom_node.hasAttribute(attr_name) and attr_value == dom_node.getAttributeNode(attr_name).value:
+                found_count += 1
+
+        if found_count == len(attributes):
+            found = True
+
+        return found
